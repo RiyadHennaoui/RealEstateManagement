8e142b0f98b9c2d30c27b1b7300f3855c831ede9
riyad <riyad.hennaoui@gmail.com>
2021-06-07 17:14:49 +0200

    Animation in splash screen added with fade in 3000ms

--- app/src/main/java/com/openclassrooms/realestatemanager/SplashScreen.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/SplashScreen.kt
@@ -2,9 +2,21 @@ package com.openclassrooms.realestatemanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
 
 class SplashScreen : AppCompatActivity() {
+    private var realEstateLogo: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        realEstateLogo = findViewById(R.id.splash_screen_iv_logo)
+        val animFadeIn: Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.fade_in)
+        realEstateLogo!!.startAnimation(animFadeIn)
+
+
     }
 }

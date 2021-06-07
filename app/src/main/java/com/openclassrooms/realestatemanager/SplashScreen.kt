a1818c41d7b1d115f717b2bc271305ca842d525c
riyad <riyad.hennaoui@gmail.com>
2021-06-07 12:07:03 +0200

    splashScreen added with colors of application and logo

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/SplashScreen.kt
@@ -0,0 +1,10 @@
+package com.openclassrooms.realestatemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

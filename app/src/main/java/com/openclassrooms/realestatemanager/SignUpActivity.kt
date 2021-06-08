7c5ad129a08531ce88f85bce1665210cac0ae4e7
riyad <riyad.hennaoui@gmail.com>
2021-06-08 11:58:55 +0200

    sign up ui finished

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.openclassrooms.realestatemanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}

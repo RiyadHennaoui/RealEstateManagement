7a271fdf1c9621c1a57c6befc54909605756d332
riyad <riyad.hennaoui@gmail.com>
2021-06-16 11:21:02 +0200

    bottom app bar and floating action botton created, but need find another icon for this

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -3,6 +3,7 @@ package com.openclassrooms.realestatemanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,12 +11,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
 
-
+        //For disable middle item in bottom app bar
         bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
 
-        bottomNavigationView.menu.getItem(2).setEnabled(false)
 
+        fab.setImageDrawable(resources.getDrawable(R.drawable.ic_add))
 
     }
 }

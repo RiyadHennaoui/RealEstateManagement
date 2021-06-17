c03c1235478cc83e21d9fc460d599c0094ee171a
riyad <riyad.hennaoui@gmail.com>
2021-06-17 10:40:19 +0200

    create the click listner on button in bottom nav bar and floating action button

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -2,23 +2,69 @@ package com.openclassrooms.realestatemanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    var bottomNavigationView: BottomNavigationView? = null
+    var fab: FloatingActionButton? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        fab = findViewById(R.id.fab)
 
         //For disable middle item in bottom app bar
-        bottomNavigationView.background = null
-        bottomNavigationView.menu.getItem(2).isEnabled = false
+        bottomNavigationView!!.background = null
+        bottomNavigationView!!.menu.getItem(2).isEnabled= false
 
+        configureBottomView()
+        configureFab()
 
-        fab.setImageDrawable(resources.getDrawable(R.drawable.ic_add))
+    }
 
+    private fun configureBottomView(){
+        bottomNavigationView?.setOnNavigationItemSelectedListener { item -> updateButtons(item.itemId) }
+    }
+
+    private fun configureFab(){
+        fab?.setOnClickListener { item -> updateButtons(item.id) }
+    }
+
+    private fun updateButtons(integer: Int): Boolean{
+        when (integer){
+
+            R.id.bottom_nav_add_property -> openAddProperty()
+            R.id.bottom_nav_list_properties -> openListProperties()
+            R.id.bottom_nav_map -> openMap()
+            R.id.bottom_nav_simulator -> openCreditSimulator()
+            R.id.fab -> openSearch()
+        }
+        return true
+    }
+
+    private fun openSearch() {
+        Toast.makeText(this,"search", Toast.LENGTH_LONG).show()
+//        TODO("Not yet implemented")
+    }
+
+    private fun openCreditSimulator() {
+        TODO("Not yet implemented")
+    }
+
+    private fun openMap() {
+        Toast.makeText(this,"openMap", Toast.LENGTH_LONG).show()
+//        TODO("Not yet implemented")
+    }
+
+    private fun openListProperties() {
+        TODO("Not yet implemented")
+    }
+
+    private fun openAddProperty() {
+        TODO("Not yet implemented")
     }
 }

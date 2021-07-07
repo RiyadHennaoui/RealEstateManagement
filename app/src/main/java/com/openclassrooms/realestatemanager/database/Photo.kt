b8da795997aa93ae3531875e32a87fdf9ec2eee1
riyad <riyad.hennaoui@gmail.com>
2021-07-07 10:20:53 +0200

    missed entity annotation for room

--- app/src/main/java/com/openclassrooms/realestatemanager/database/Photo.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/Photo.kt
@@ -1,7 +1,9 @@
 package com.openclassrooms.realestatemanager.database
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity(tableName = "photo")
 data class Photo (
     @PrimaryKey(autoGenerate = true)
     val id: Int,

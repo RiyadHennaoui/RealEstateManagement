d692ad30f5aefea60b242d5796fc7d03ac19177c
riyad <riyad.hennaoui@gmail.com>
2021-08-26 15:42:47 +0200

    view binding added in SignUpActivity

--- app/src/main/java/com/openclassrooms/realestatemanager/SignUpActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/SignUpActivity.kt
@@ -6,36 +6,36 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.lifecycle.ViewModelProvider
+import com.openclassrooms.realestatemanager.databinding.ActivitySignInBinding
+import com.openclassrooms.realestatemanager.databinding.ActivitySignUpBinding
 import com.openclassrooms.realestatemanager.repositories.UserRepository
 import com.openclassrooms.realestatemanager.viewmodel.UserViewModel
 import com.openclassrooms.realestatemanager.viewmodel.UserViewModelFactory
 
 class SignUpActivity : AppCompatActivity() {
-
+    lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
-        val etEmail: EditText = findViewById(R.id.sign_up_activity_et_email)
-        val etPassword: EditText = findViewById(R.id.sign_up_activity_et_password)
-        val etDisplayName: EditText = findViewById(R.id.sign_up_activity_et_name)
-        val btnRegister: ImageButton = findViewById(R.id.sign_up_activity_btn_validate)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
 
         val userRepository = UserRepository()
         val factory = UserViewModelFactory(userRepository)
-
         val userViewModel = ViewModelProvider(this, factory).get(UserViewModel::class.java)
 
-        btnRegister.setOnClickListener {
+        binding.signUpActivityBtnValidate.setOnClickListener {
 
-            val email = etEmail.text.toString()
-            val password = etPassword.text.toString()
-            val displayName = etDisplayName.text.toString()
+            val email = binding.signUpActivityEtEmail.text.toString()
+            val password = binding.signUpActivityEtPassword.text.toString()
+            val displayName = binding.signUpActivityEtName.text.toString()
             //TODO ajouter la gestion des photos e tcheck si l'email et le password sont bon
 
             when {
-                !isEmailValid(email) -> etEmail.error = "Email not valid"
-                password.length < 3 -> etPassword.error = "password can not empty or under 3 characters"
+                !isEmailValid(email) -> binding.signUpActivityEtEmail.error = "Email not valid"
+                password.length < 3 -> binding.signUpActivityEtPassword.error = "password can not empty or under 3 characters"
                 isEmailValid(email) && password.isNotEmpty() && displayName.isNotEmpty() ->
                     if (email.isNotEmpty() && password.isNotEmpty() && displayName.isNotEmpty()) {
 
